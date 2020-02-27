e4ea00a2685d34809577ab23617d427f387e5f15
Adam Vinueza <adam.vinueza@mindflash.com>
2020-02-26 17:48:11 -0700

    added a simple extra endpoint

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const sample = require('./sample/send-json');
 const PORT = 3000;
 
 app.get('/', async (req, res) => {
   res.send('Hello, World!');
 });
+
+app.use('/sample', sample);
 
 const server = app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
